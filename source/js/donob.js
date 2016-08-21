919ed5b155a5fd22bc1a9914835280ca99eb61b8
LoeiFy <loeify@gmail.com>
2016-08-21 22:03:48 +0800

    hover

--- source/js/donob.js
+++ source/js/donob.js
@@ -1,8 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+    var images = [];
+
     document.querySelector('#posts').addEventListener('mouseover', function(e) {
 
         if (e.target.tagName == 'A') {
+
             var thumb = document.querySelector('#thumb');
             var image = e.target.getAttribute('data-thumb');
 
@@ -14,16 +17,32 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
             if (thumb) {
-                if (thumb.src == image) {
+                if (thumb.getAttribute('data-thumb') == image) {
                     return
                 }
                 document.body.removeChild(thumb)
             }
 
-            var img = document.createElement('img');
-            img.id = 'thumb';
-            img.src = image;
-            document.body.insertBefore(img, document.body.firstChild)
+            var dimg = document.createElement('img');
+            dimg.id = 'thumb';
+            dimg.setAttribute('data-thumb', image)
+            dimg.style.opacity = 0;
+            document.body.insertBefore(dimg, document.body.firstChild)
+
+            if (images.indexOf(image) > -1) {
+                dimg.style.opacity = 1;
+                dimg.src = image;
+            } else {
+
+                var img = new Image();
+                img.onload = function() {
+                    images.push(image)
+                    dimg.style.opacity = 1;
+                    dimg.src = image;
+                }
+                img.src = image
+
+            }
         }
 
     })
