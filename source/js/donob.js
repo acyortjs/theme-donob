97c16fa91cf0817d3a5b1856e44784e24edb6a94
LoeiFy <loeify@gmail.com>
2016-08-21 21:06:44 +0800

    mouse move image

--- source/js/donob.js
+++ source/js/donob.js
@@ -1,1 +1,19 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+    document.querySelector('#posts').addEventListener('mouseover', function(e) {
+        if (e.target.tagName == 'A' && e.target.getAttribute('data-thumb')) {
+            var oldimg = document.querySelector('#thumb');
+            if (oldimg) {
+                document.body.removeChild(oldimg)
+            }
+
+            var img = document.createElement('img');
+            img.id = 'thumb';
+            img.onload = function() {
+                document.body.insertBefore(img, document.body.firstChild)
+            }
+            img.src = e.target.getAttribute('data-thumb'); 
+        }
+    })
+
+})
